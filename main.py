0532c6ed781059c777840b7cd2783aac9d77ce5e
shipkovandrej <92599020+shipkovandrej@users.noreply.github.com>
2022-04-16 08:55:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import module
+from figures import circle_area
+from figures import triangle_area
+#print(dir(module))
+#print(module.__name__)
+#module.get_set()
+#module.get_txt()
+#print(module.list_doc.doc)
+#print(module.file_doc.doc)
+
+triangle_area()
